a96939655109cb0e5f2c4a8b958bf6dd13739b8e
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-09-25 16:56:27 +0530

    Chore(api) : Connected to MongoDB & also added nodemon,mongoose and dotenv as dependency

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
+
+//DB Configuration
+const DB_url =  process.env.MONGO_URL
+mongoose.connect(DB_url)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.listen(PORT,()=>{
     console.log("==================================")
