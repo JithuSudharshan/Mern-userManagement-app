d96e63e81681cac5fd0ec9a29e8a47df8ca63187
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-09-25 13:40:21 +0530

    Feat(client/user) : Routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 const App = () => {
-  return (
-    <div >Jithu Sudharshan</div>
+  return ( 
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage/>} />
+      <Route path="/sign-In" element={<SignIn/>} />
+      <Route path="/sign-Up" element={<SignUp/>} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 
