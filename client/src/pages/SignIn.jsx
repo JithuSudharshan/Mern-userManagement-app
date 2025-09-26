f9fc8e12f8f154779008ce05849d4fd41e37b458
Jithu Sudharshan <jithusudharshan@gmail.com>
2025-09-26 22:33:22 +0530

    Chore(api/user) = Added Redux Store ,added reduxToolkit and react-redux as dependencies and implemented JWT

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,47 +1,51 @@
-import { useState } from "react"
-import {Link, useNavigate} from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const navigate = useNavigate();
+  const { loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-
-  const [formData,setFormData] = useState({})
-  const [loading,setloading]= useState(false)
-  const [error,setError]= useState(false)
-  const navigate = useNavigate()
-
-
-  const handleChange = (e)=>{
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]:e.target.value
-    })
-  }
+      [e.target.id]: e.target.value,
+    });
+  };
 
-
-  const handleSubmit= async(e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      setloading(true)
-      setError(false)
-      const res = await fetch("/api/auth/signin",{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify(formData)
-    })
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    setloading(false)
-    if(data.sucess === false){
-      setError(true)
-      return;
+      const data = await res.json();
+      console.log(data);
+
+      if (data.sucess === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (err) {
+      dispatch(signInFailure(err));
     }
-    } catch (error) {
-      setloading(false)
-    }
-     navigate("/")
-  }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
@@ -64,18 +68,20 @@ const SignIn = () => {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading..." : "Sign in" }
+          {loading ? "Loading..." : "Sign in"}
         </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Dont't have an account</p>
         <Link to="/sign-up">
-          <span className="text-blue-500" >Sign up</span>
+          <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className="text-red-600 mt-3">{error && "Something went wrong...!"}</p>
+      <p className="text-red-600 mt-3">
+        {error ? error || "Something went wrong...!" : ""}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
